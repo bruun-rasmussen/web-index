461162ea1588bee9fca24fadde57e35b8f913143
Ole Sandum <ole@sandum.net>
2019-02-19 12:44:02 +0100

    added a few more (failing) test cases

--- src/test/java/dk/es/lucene/LuceneIndexTest.java
+++ src/test/java/dk/es/lucene/LuceneIndexTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 import org.yaml.snakeyaml.Yaml;
 
@@ -14,16 +15,15 @@ import org.yaml.snakeyaml.Yaml;
  */
 public class LuceneIndexTest
 {
-  
   public LuceneIndexTest()
   {
   }
-    
-  private void readYaml(InputStream is, LuceneIndex index) throws IOException {
+
+  private static void readYaml(InputStream is, LuceneIndex index) throws IOException {
     Yaml yaml = new Yaml();
-    
+
     Map<Integer, List<Map<String,String>>> obj = (Map<Integer, List<Map<String,String>>>)yaml.load(is);
-    
+
     LuceneIndex.Writer w = index.open(true);
     for (Map.Entry<Integer, List<Map<String,String>>> k : obj.entrySet())
     {
@@ -37,20 +37,55 @@ public class LuceneIndexTest
     }
     w.close();
   }
-  
+
+  private LuceneIndex readIndex(String yamlResource) throws IOException {
+    String lang = yamlResource.replaceFirst(".*_([a-z]{2})\\.yaml", "$1");
+
+    LuceneIndex idx = LuceneIndex.RAM(new Locale(lang));
+    try (InputStream is = getClass().getResourceAsStream(yamlResource))
+    {
+      readYaml(is, idx);
+    }
+    return idx;
+  }
+
+  private LuceneIndex da;
+
+  @Before
+  public void loadIndex() throws IOException {
+    da = readIndex("sample_da.yaml");
+  }
+
   @Test
-  public void testLoadYaml() throws IOException
+  public void testSimple()
   {
-    LuceneIndex da = LuceneIndex.RAM(new Locale("da"));        
-    InputStream is = getClass().getResourceAsStream("sample_da.yaml");
-    assertNotNull(is);
-    readYaml(is, da);
-    is.close();
-    
     assertEquals(0, da.search("nevermind", "description").size());
     assertEquals(1, da.search("Rød begyndelse", "description").size());
     assertEquals(3, da.search("Knud Nielsen", "description").size());
+
+    assertEquals(1, da.search("samlerobjekt", "description").size());
+  }
+
+  @Test
+  public void testComposite()
+  {
     assertEquals(2, da.search("kontorstol", "description").size());
     assertEquals(2, da.search("stol", "description").size());
+  }
+
+  @Test
+  public void testPlural()
+  {
+    // "Et par skamler"
+    assertEquals(1, da.search("skamler", "description").size());
+    assertEquals(1, da.search("skammel", "description").size());
+  }
+
+  @Test
+  public void testSynonym()
+  {
+    // "Bogkasse og vitrine"
+    assertEquals(1, da.search("vitrine", "description").size());
+    assertEquals(1, da.search("skab", "description").size());
   }
 }
